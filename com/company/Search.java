b052047db8f01557ec0f6532b39cdab03bf198d2
Mukhamedyakub <230198@astanait.edu.kz>
2024-05-24 21:40:51 +0500

    Created Search

--- com/company/Search.java
+++ com/company/Search.java
@@ -23,3 +23,30 @@ public abstract class Search<T> {
         edgeTo = new HashMap<>();
     }
 
+    /**
+     * Checks if there is a path to the specified vertex.
+     *
+     * @param v The vertex to check.
+     * @return True if there is a path to the vertex, otherwise false.
+     */
+    public boolean hasPathTo(Vertex<T> v) {
+        return marked.contains(v);
+    }
+
+    /**
+     * Gets the path to the specified vertex.
+     *
+     * @param v The vertex.
+     * @return The iterable path to the vertex.
+     */
+    public Iterable<T> pathTo(T v) {
+        Vertex<T> vertex = new Vertex<>(v);
+        if (!hasPathTo(vertex)) return null;
+        LinkedList<T> ls = new LinkedList<>();
+        for (Vertex<T> i = vertex; i != source; i = edgeTo.get(i)) {
+            ls.push(i.getData());
+        }
+        ls.push(source.getData());
+        return ls;
+    }
+}
